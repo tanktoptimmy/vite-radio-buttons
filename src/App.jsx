348ff18dc8b565b07acbb2a2eec92577d3e42fd8
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-11-22 08:40:48 +0000

    adding extra button and text

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,34 @@ function App() {
 
   return (
     <div className="App">
-      <input 
-        type="radio" name="emotion" 
-        id="sad" class="input-hidden" />
-      <label for="sad">
-        <img className="selected" src={tick}/>
-        <img className="notSelected" src={reactLogo}/>
-      </label>
-
-      <input 
-        type="radio" name="emotion"
-        id="happy" class="input-hidden" />
-      <label for="happy">
+      <div className="holder">
+        <input 
+          type="radio" name="emotion" 
+          id="sad" class="input-hidden" />
+        <label for="sad">
           <img className="selected" src={tick}/>
           <img className="notSelected" src={reactLogo}/>
-      </label>
+          <p>Boom</p>
+        </label>
+
+        <input 
+          type="radio" name="emotion"
+          id="happy" class="input-hidden" />
+        <label for="happy">
+            <img className="selected" src={tick}/>
+            <img className="notSelected" src={reactLogo}/>
+            <p>Bang</p>
+        </label>
+
+        <input
+          type="radio" name="emotion"
+          id="bottom" class="input-hidden" />
+        <label for="bottom">
+            <img className="selected" src={tick}/>
+            <img className="notSelected" src={reactLogo}/>
+            <p>Wallop</p>
+        </label>
+      </div>
     </div>
   )
 }
